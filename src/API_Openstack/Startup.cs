866726a7fe4818842139c37e85be41b531ed4d55
guadalupe-gomez <guada.sgomez@gmail.com>
2016-12-28 12:38:06 -0300

    Se deja solo lo de api, quitando la parte de ejemplo

--- src/API_Openstack/Startup.cs
+++ src/API_Openstack/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -65,7 +61,7 @@ namespace API_Openstack
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Instancias}/{action=Index}/{id?}");
             });
         }
     }
